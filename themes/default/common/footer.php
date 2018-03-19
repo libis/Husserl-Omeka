33aef628cc64409661a2d7f19e9a7e58e5e4a9a7
Joris <joris.lambrechts@kuleuven.be>
2018-03-19 13:43:12 +0100

    footer adjustment

--- themes/default/common/footer.php
+++ themes/default/common/footer.php
@@ -3,17 +3,14 @@
             <div id="footer-text">
                 <?php echo get_theme_option('Footer Text'); ?>
                 <div class="row no-gutters">
-
                   <div class="col-xs-12">
                         <div>
                             <a href="http://kuleuven.be"><img src="<?php echo img("KULEUVEN.png");?>"></a>
                             <a href="http://libis.be"><img src="<?php echo img("libis_gray.png");?>"></a>
                         </div>
                         <div class="footer-brand">
-
-
                           <div class="footer-content">
-                            <p><a class="footer-logo" href="<?php echo url("/");?>">digital<span>Husserl</span></a> Straatstraat 22 | 3000 Leuven | +3216222222</p>
+                            <!--<p><a class="footer-logo" href="<?php echo url("/");?>">digital<span>Husserl</span></a> Straatstraat 22 | 3000 Leuven | +3216222222</p>-->
                             <ul>
                                 <li><a href="">Menu item 1</a></li>
                                 <li><a href="">Menu item 1</a></li>
@@ -21,6 +18,9 @@
                                 <li><a href="">Menu item 1</a></li>
                             </ul>
                           </div>
+                        </div>
+                        <div class="copyright">
+                          © KU Leuven
                         </div>
                   </div>
                 </div>
